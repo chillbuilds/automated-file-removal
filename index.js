f4affcf3bcb99979cd7b497b189de2d35b8578fe
heliumface770 <heliumface770@gmail.com>
2019-11-21 22:46:06 -0700

    stas.atime revelation

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const fs = require("fs");
+const moment = require("moment");
 const fileArr = [];
-const dir = "../test-images/"; //place directory location between quotes
+const dir = "./test-files/";
+// const dir = `C:/Users/Master William/Pictures/`;
+const time = moment().format();
+const monthAgo = moment().subtract(30, 'days').format();
+
 //gather file names from 'order assets' and push into array
 fs.readdirSync(dir).forEach(file => {
     fileArr.push(file);
   });
-console.log(fileArr);
-  for(var i = 1; i < fileArr.length; i++){
-    fs.stat(`${dir}${fileArr[i]}`, function(err, stats) {
-        if(stats.isFile() === true)
+  
+//loop through files in dir
+  for(var i = 0; i < fileArr.length; i++){
+      let x = fileArr[i];
+    fs.stat(`${dir}${fileArr[i]}`, function(err, stats, i) {
+        if(stats.isFile() === true){
         console.log(stats.atime);
+        console.log(x);
+    }
+        //stats.atime
     });
   }
+
+console.log(time)
+console.log(monthAgo);
