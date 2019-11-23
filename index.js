d14368db05a7299c99bbe5110c7beebc2f1d6c47
heliumface770 <heliumface770@gmail.com>
2019-11-22 20:50:33 -0700

    added file deletion functionality - ready for testing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fileArr = [];
 const dir = "./test-files/";
 // const dir = "";
 var date = new Date();
-const month = date.getMonth();
+const month = date.getMonth()+1;
 const day = date.getDate();
 
 //gather file names from 'order assets' and push into array
@@ -13,23 +13,20 @@ fs.readdirSync(dir).forEach(file => {
 
 //loop through files in dir
   for(var i = 0; i < fileArr.length; i++){
+    let filePath = dir+fileArr[i];
     fs.stat(`${dir}${fileArr[i]}`, function(err, stats) {
       if(stats.isFile() === true){
-        let imgMonth = stats.atime.getMonth();
+        let imgMonth = stats.atime.getMonth()+1;
         let imgDay = stats.atime.getDate();
+        console.log(imgMonth);
+        console.log(imgDay);
         if(imgMonth === month){return}
         else{
-          
+         if(imgDay < day){
+          fs.unlinkSync(filePath);
+         }
         }
       }
     });
   }
 console.log(month + "-" + day);
-
-let expDate = month - 1;
-console.log(expDate);
-
-// let expDate = day - 30;
-// if(expDate < 1){
-//   expDate = expDate - expDate * 2; //format returned expiration date;
-// }
